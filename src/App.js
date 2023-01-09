eaa42a72d9bc4046a0d29157eaa210d5b173218f
jothishwar <jothishwarvvr@gmail.com>
2023-01-09 13:48:25 +0530

    completed home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Button } from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons'
 
 function App() {
   return (
-    <div className="App">
-      Quiz app
+    <div className="app">
+      <h1>Quiz app </h1>
+      <p>Click the button below to start the quiz. All the best! </p>
+      <Button 
+        colorScheme='whatsapp' 
+        rightIcon={<ArrowForwardIcon />} 
+        variant='outline'
+        onClick={()=>console.log("s")}
+      >
+        Start Quiz
+      </Button>
     </div>
   );
 }
